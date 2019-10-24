878a05ba666f487008b152d65b4cf0a55e39dddb
vagrant <mikefyke@hotmail.com>
2019-10-24 19:38:17 +0000

    Cleaned Logic and added red cube params

--- scripts/logic.js
+++ scripts/logic.js
@@ -7,6 +7,11 @@ var topXCorner = 20;
 const xthick = 800;
 const ythick = 450;
 
+var redTopY = 120;
+var redTopX = 250;
+const redXthick = 400;
+const redYthick = 400;
+
 
 function calculateMousePos(evt){      // an event fires when mouse moves
     var rect = canvas.getBoundingClientRect();
@@ -29,6 +34,8 @@ window.onload = function() {
         var mousePos = calculateMousePos(evt);
         topCorner = mousePos.y-(ythick/2); // paddle aligns in centre with mouse
         topXCorner = mousePos.x-(xthick/2);
+        redTopY = mousePos.y - 200;
+        redTopX = mousePos.x - 500;
        });
 }
 
@@ -70,90 +77,34 @@ function drawEverything() {
     ctx.lineTo(topXCorner + xthick,topCorner +ythick);
     ctx.strokeStyle = "grey";
     ctx.stroke();
+
+    ctx.fillStyle = "blue"; //
+    ctx.fillRect(topXCorner + 270,topCorner + 170,100,100);
+
+    ctx.fillStyle = "red"; //
+    ctx.fillRect(redTopX,redTopY,redXthick,redYthick);
+    console.log("topXCorner",topXCorner)
+    console.log("topYCorner",topCorner)
     } 
+    if(redTopX < 30) {
+        redTopX = 30;
+    }
+    if (redTopX >= 350){
+        redTopX = 350;
+    }
+    if (redTopY < 35){
+        redTopY = 35;
+    }
+    if (redTopY > 150){
+        redTopY = 150;
+    }
 ////////////////////////////// Left Boundry - STOP
         if (topXCorner <= 50){
 
             topXCorner = 50 ;
     
-            ctx.fillStyle = "black";
-            ctx.fillRect(0,0,canvas.width,canvas.height);       // blanks screen black
-
-            ctx.fillStyle = "grey";
-            ctx.fillRect(topXCorner,topCorner,xthick,ythick);
-
-            ctx.fillStyle = "black";
-            ctx.fillRect(topXCorner +5,topCorner +5 ,790,440);
-
-            ctx.beginPath();
-            ctx.lineWidth = 2;
-            ctx.moveTo(0,0);
-            ctx.lineTo(topXCorner,topCorner);
-            ctx.strokeStyle = "grey";
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.moveTo(1400,0);
-            ctx.lineTo(topXCorner + xthick,topCorner);
-            ctx.strokeStyle = "grey";
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.moveTo(0,720);
-            ctx.lineTo(topXCorner,topCorner + ythick);
-            ctx.strokeStyle = "grey";
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.moveTo(1400,720);
-            ctx.lineTo(topXCorner + xthick,topCorner +ythick);
-            ctx.strokeStyle = "grey";
-            ctx.stroke();
-    ////////////////////////////// Top Boundry - Stop
-    } if (topCorner <= 50){
-
-        topCorner = 50 ;
-        
-        ctx.fillStyle = "black";
-        ctx.fillRect(0,0,canvas.width,canvas.height);       // blanks screen black
-    
-        ctx.fillStyle = "grey";
-        ctx.fillRect(topXCorner,topCorner,xthick,ythick);
-    
-        ctx.fillStyle = "black";
-        ctx.fillRect(topXCorner +5,topCorner +5 ,790,440);
-    
-        ctx.beginPath();
-        ctx.lineWidth = 2;
-        ctx.moveTo(0,0);
-        ctx.lineTo(topXCorner,topCorner);
-        ctx.strokeStyle = "grey";
-        ctx.stroke();
-    
-        ctx.beginPath();
-        ctx.moveTo(1400,0);
-        ctx.lineTo(topXCorner + xthick,topCorner);
-        ctx.strokeStyle = "grey";
-        ctx.stroke();
-    
-        ctx.beginPath();
-        ctx.moveTo(0,720);
-        ctx.lineTo(topXCorner,topCorner + ythick);
-        ctx.strokeStyle = "grey";
-        ctx.stroke();
-    
-    
-        ctx.beginPath();
-        ctx.moveTo(1400,720);
-        ctx.lineTo(topXCorner + xthick,topCorner +ythick);
-        ctx.strokeStyle = "grey";
-        ctx.stroke();
-        
-        ////////////////////////////// Top Boundry - Print Normally
-        } if(topCorner > 50 ) {        
-            console.log("Drawing Back");
             ctx.fillStyle = "black"; // blanks screen black
-            ctx.fillRect(0,0,canvas.width,canvas.height);       
+            ctx.fillRect(0,0,canvas.width,canvas.height);
             ctx.fillStyle = "grey"; //
             ctx.fillRect(topXCorner,topCorner,xthick,ythick);
             ctx.fillStyle = "black"; //
@@ -174,17 +125,24 @@ function drawEverything() {
             ctx.lineTo(topXCorner,topCorner + ythick);
             ctx.strokeStyle = "grey";
             ctx.stroke();
-            ctx.beginPath(); //
+            ctx.beginPath();  //
             ctx.moveTo(1400,720);
             ctx.lineTo(topXCorner + xthick,topCorner +ythick);
             ctx.strokeStyle = "grey";
             ctx.stroke();
-            }
 
-////////////////////////////// Right Boundry - Print Normally
-        if(topXCorner < 550 ) {        
-        console.log("Drawing Back");
-        ctx.fillStyle = "black";// blanks screen black
+            ctx.fillStyle = "blue"; //
+            ctx.fillRect(topXCorner + 270,topCorner + 170,100,100);
+
+            ctx.fillStyle = "red"; //
+            ctx.fillRect(redTopX,redTopY,redXthick,redYthick);
+        } 
+////////////////////////////// Top Boundry - Stop
+        if (topCorner <= 50) {
+
+        topCorner = 50 ;
+        
+        ctx.fillStyle = "black";  // blanks screen black
         ctx.fillRect(0,0,canvas.width,canvas.height);
         ctx.fillStyle = "grey"; //
         ctx.fillRect(topXCorner,topCorner,xthick,ythick);
@@ -210,9 +168,18 @@ function drawEverything() {
         ctx.moveTo(1400,720);
         ctx.lineTo(topXCorner + xthick,topCorner +ythick);
         ctx.strokeStyle = "grey";
-        ctx.stroke();
-////////////////////////////// Right Boundry - STOP
-        } if (topXCorner >= 550){
+        ctx.stroke();    
+        
+        ctx.fillStyle = "blue"; //
+        ctx.fillRect(topXCorner + 270,topCorner + 170,100,100);
+        if(redTopY <= 20) {
+            redTopY = 20;
+        }
+        ctx.fillStyle = "red"; //
+        ctx.fillRect(redTopX,redTopY,redXthick,redYthick);
+        }
+////////////////////////////// Right Boundry - Stop
+        if (topXCorner >= 550){
     
             topXCorner = 550 ;
             
@@ -243,6 +210,12 @@ function drawEverything() {
             ctx.lineTo(topXCorner + xthick,topCorner +ythick);
             ctx.strokeStyle = "grey";
             ctx.stroke();
+
+            ctx.fillStyle = "blue"; //
+            ctx.fillRect(topXCorner + 270,topCorner + 170,100,100);
+
+            ctx.fillStyle = "red"; //
+            ctx.fillRect(redTopX,redTopY,redXthick,redYthick);
         }
 ////////////////////////////// Bottom Boundry - Stop
         if (topCorner >= 240){
@@ -276,6 +249,12 @@ function drawEverything() {
             ctx.lineTo(topXCorner + xthick,topCorner +ythick);
             ctx.strokeStyle = "grey";
             ctx.stroke();
+
+            ctx.fillStyle = "blue"; //
+            ctx.fillRect(topXCorner + 270,topCorner + 170,100,100);
+
+            ctx.fillStyle = "red"; //
+            ctx.fillRect(redTopX,redTopY,redXthick,redYthick);
         } 
 
 
