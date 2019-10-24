b3744cdabdc0f586ff8909003c4c2187335fbba4
vagrant <mikefyke@hotmail.com>
2019-10-24 15:30:41 +0000

    xaxis tracking

--- scripts/logic.js
+++ scripts/logic.js
@@ -3,6 +3,7 @@ var ctx = canvas.getContext('2d');
 var framesPerSecond = 30;
 
 var topCorner = 20;
+var topXCorner = 20;
 const xthick = 800;
 const ythick = 450;
 
@@ -27,7 +28,8 @@ window.onload = function() {
       function(evt) {
         var mousePos = calculateMousePos(evt);
         topCorner = mousePos.y-(ythick/2); // paddle aligns in centre with mouse
-      });
+        topXCorner = mousePos.x-(xthick/2);
+       });
 }
 
 function drawEverything() {
@@ -36,34 +38,34 @@ function drawEverything() {
     ctx.fillRect(0,0,canvas.width,canvas.height);       // blanks screen black
     
     ctx.fillStyle = "grey";
-    ctx.fillRect(20,topCorner,xthick,ythick);
+    ctx.fillRect(topXCorner,topCorner,xthick,ythick);
 
     ctx.fillStyle = "black";
-    ctx.fillRect(25,topCorner +5 ,790,440);
+    ctx.fillRect(topXCorner +5,topCorner +5 ,790,440);
 
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.moveTo(0,0);
-    ctx.lineTo(20,topCorner);
+    ctx.lineTo(topXCorner,topCorner);
     ctx.strokeStyle = "grey";
     ctx.stroke();
 
     ctx.beginPath();
     ctx.moveTo(1400,0);
-    ctx.lineTo(820,topCorner);
+    ctx.lineTo(topXCorner + xthick,topCorner);
     ctx.strokeStyle = "grey";
     ctx.stroke();
 
     ctx.beginPath();
     ctx.moveTo(0,720);
-    ctx.lineTo(20,topCorner + ythick);
+    ctx.lineTo(topXCorner,topCorner + ythick);
     ctx.strokeStyle = "grey";
     ctx.stroke();
 
 
     ctx.beginPath();
     ctx.moveTo(1400,720);
-    ctx.lineTo(820,topCorner +ythick);
+    ctx.lineTo(topXCorner + xthick,topCorner +ythick);
     ctx.strokeStyle = "grey";
     ctx.stroke();
   }
