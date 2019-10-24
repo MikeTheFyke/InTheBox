9fbbf6c5bfe2fbae4403cc90bc02739db933f7f5
vagrant <mikefyke@hotmail.com>
2019-10-24 15:21:21 +0000

    tracking mouse movement and floor yaxis

--- scripts/logic.js
+++ scripts/logic.js
@@ -2,44 +2,68 @@ var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext('2d');
 var framesPerSecond = 30;
 
+var topCorner = 20;
+const xthick = 800;
+const ythick = 450;
+
+
+function calculateMousePos(evt){      // an event fires when mouse moves
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+      x:mouseX,   // Object Literal
+      y:mouseY    // Object Literal
+    };
+  }
+
 window.onload = function() {
     setInterval(function() {
     drawEverything();
     },1000/framesPerSecond); // Hundredth of seconds, ballX movement
+
+    canvas.addEventListener ('mousemove',   // keypress, mouseclick, mousemove
+      function(evt) {
+        var mousePos = calculateMousePos(evt);
+        topCorner = mousePos.y-(ythick/2); // paddle aligns in centre with mouse
+      });
 }
 
 function drawEverything() {
     console.log("Drawing Back");
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,canvas.width,canvas.height);       // blanks screen black
+    
     ctx.fillStyle = "grey";
-    ctx.fillRect(90,50,120,60);
+    ctx.fillRect(20,topCorner,xthick,ythick);
+
     ctx.fillStyle = "black";
-    ctx.fillRect(92,52,116,56);
+    ctx.fillRect(25,topCorner +5 ,790,440);
 
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.moveTo(0,0);
-    ctx.lineTo(90,50);
+    ctx.lineTo(20,topCorner);
     ctx.strokeStyle = "grey";
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(300,0);
-    ctx.lineTo(210,50);
+    ctx.moveTo(1400,0);
+    ctx.lineTo(820,topCorner);
     ctx.strokeStyle = "grey";
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(0,150);
-    ctx.lineTo(90,110);
+    ctx.moveTo(0,720);
+    ctx.lineTo(20,topCorner + ythick);
     ctx.strokeStyle = "grey";
     ctx.stroke();
 
 
     ctx.beginPath();
-    ctx.moveTo(300,150);
-    ctx.lineTo(210,110);
+    ctx.moveTo(1400,720);
+    ctx.lineTo(820,topCorner +ythick);
     ctx.strokeStyle = "grey";
     ctx.stroke();
   }
