a75432a6b102b3ef94c8f91e048cda35dd7a6119
vagrant <mikefyke@hotmail.com>
2019-11-20 20:48:29 +0000

    Addedmore box logic

--- scripts/storeLogic.js
+++ scripts/storeLogic.js
@@ -17,17 +17,17 @@ var ct = canvas02.getContext('2d');
     var box01Y4 = backY4;
 
 //// Box 02
-    var box02X1 = backX1 - ((backX1 - frontX1)/4);
-    var box02Y1 = frontY4 - (((frontY4 - frontY1)/4)+30);
+    var box02X1 = frontX1 + (((backX1 - frontX1)/gridCtr)*6)
+    var box02Y1 = frontY4 - (((frontY4 - backY4)/gridCtr)*15);
 
-    var box02X2 = (box02X1 + ((frontX2 - frontX1)/4));
-    var box02Y2 = box02Y1;
+    var box02X2 = box02X1 + (((frontX2 - frontX1)/4)-35);
+    var box02Y2 = frontY4 - (((frontY4 - backY4)/gridCtr)*15);
 
-    var box02X3 = (box02X1 + ((frontX2 - frontX1)/4));
-    var box02Y3 = box02Y1 + 95;
+    var box02X3 = box02X1 + (((frontX2 - frontX1)/4)-35);
+    var box02Y3 = frontY4 - (((frontY4 - backY4)/gridCtr)*6);
 
-    var box02X4 = box02X1;
-    var box02Y4 = box02Y1 + 95;
+    var box02X4 = frontX1 + (((backX1 - frontX1)/gridCtr)*6);
+    var box02Y4 = frontY4 - (((frontY4 - backY4)/gridCtr)*6);
 ////
     var boxCtr = document.getElementById("boxCount").value;
 
@@ -41,14 +41,23 @@ var ct = canvas02.getContext('2d');
             ct.lineTo(box01X4 + (((backX2 - backX1)/4) * i) , box01Y4);
             ct.closePath();
             ct.stroke();
-            ct.strokeStyle = "orange"; // front side face
-            ct.beginPath();
-            ct.moveTo((box02X1 + (((frontX2 - frontX1)/4) * i)-35) , box02Y1);
-            ct.lineTo((box02X2 + (((frontX2 - frontX1)/4) * i)-35) , box02Y2);
-            ct.lineTo((box02X3 + (((frontX2 - frontX1)/4) * i)-35) , box02Y3);
-            ct.lineTo((box02X4 + (((frontX2 - frontX1)/4) * i)-35) , box02Y4);
-            ct.closePath();
-            ct.stroke();
+            ct.strokeStyle = "yellow"; // front side face
+            ctx.beginPath();
+            ctx.moveTo(box02X1 + (((frontX2 - frontX1)/4) * i),box02Y1);
+            ctx.lineTo(box02X2 + (((frontX2 - frontX1)/4) * i),box02Y2);
+            ctx.lineTo(box02X3 + (((frontX2 - frontX1)/4) * i),box02Y3);
+            ctx.lineTo(box02X4 + (((frontX2 - frontX1)/4) * i), box02Y4);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.strokeStyle = "green"; ///Vertical
+            ctx.beginPath();
+            ctx.moveTo(box01X2 + (((backX2 - backX1)/4) * i),box01Y2);
+            ctx.lineTo(box02X2 + (((frontX2 - frontX1)/4) * i),box02Y2);
+            ctx.lineTo(box02X3 + (((frontX2 - frontX1)/4) * i),box02Y3);
+            ct.lineTo(box01X3 + (((backX2 - backX1)/4) * i) , box01Y3);
+            ctx.closePath();
+            ctx.stroke();
+            
         }
     }
 }
