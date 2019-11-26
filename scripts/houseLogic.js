e64fc22dffb6b0db377d08cefb5bccc4ae158faf
vagrant <mikefyke@hotmail.com>
2019-11-26 15:52:37 +0000

    house logic adjusted

---
+++ scripts/houseLogic.js
@@ -0,0 +1,141 @@
+var canvas = document.getElementById('houseCanvas');
+var ctx = canvas.getContext('2d');
+var framesPerSecond = 30;
+
+var Xcord = 0;
+var Ycord = 0;
+var topYCorner;
+var topXCorner;
+const Xwidth = 700;
+const Yheight = 400;
+
+function calculateMousePos(evt){      // an event fires when mouse moves
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+      x:mouseX,   // Object Literal
+      y:mouseY    // Object Literal
+    };
+  }
+
+window.onload = function() {
+    setInterval(function() {
+    drawEverything();
+    },1000/framesPerSecond); // Hundredth of seconds, ballX movement
+
+    canvas.addEventListener ('mousemove',   // keypress, mouseclick, mousemove
+      function(evt) {
+        var mousePos = calculateMousePos(evt);
+        Xcord = mousePos.x;
+        Ycord = mousePos.y;
+        topYCorner = mousePos.y-(Yheight/2); // paddle aligns in centre with mouse
+        topXCorner = mousePos.x-(Xwidth/2);
+       });
+}
+
+function drawEverything() {
+    ctx.fillStyle = "black"; // Canvas Black
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+            if (topYCorner <= 90){ // Top Boundry
+                topYCorner = 90 ;}
+            if (topYCorner >= 140){ // Bottom Boundry
+                topYCorner = 140 ;} 
+            if (topXCorner >= 150){ // Right Boundry
+                topXCorner = 150 ;}
+            if (topXCorner <= 50){ // Left Boundry
+                topXCorner = 50 ;}
+
+    ctx.strokeStyle = "grey";
+    ctx.lineWidth = 2;
+
+    ctx.beginPath(); // Top Left Line
+    ctx.moveTo(topXCorner,topYCorner);
+    ctx.lineTo(topXCorner + (Xwidth/2),topYCorner - 50); // Peak
+    ctx.lineTo(topXCorner + Xwidth,topYCorner);
+    ctx.lineTo(topXCorner + Xwidth,topYCorner + Yheight);
+    ctx.lineTo(topXCorner,topYCorner + Yheight);
+    ctx.lineTo(topXCorner,topYCorner);
+    ctx.stroke();
+
+    ctx.beginPath(); // Top Left Line
+    ctx.moveTo(0,0);
+    ctx.lineTo(topXCorner,topYCorner);
+    ctx.stroke();
+
+    ctx.beginPath(); // Top Right Line
+    ctx.moveTo(900,0);
+    ctx.lineTo(topXCorner + Xwidth,topYCorner);
+    ctx.stroke();
+
+    ctx.beginPath(); // Bottom Leftt Line
+    ctx.moveTo(0,600);
+    ctx.lineTo(topXCorner,topYCorner + Yheight);
+    ctx.stroke();
+
+    ctx.beginPath(); // Bottom Right Line
+    ctx.moveTo(900,600);
+    ctx.lineTo(topXCorner + Xwidth,topYCorner + Yheight);
+    ctx.stroke();
+
+    var backX1 = topXCorner + 50;
+    var backY1 = topYCorner + 30;
+
+/// Back Walls
+ctx.beginPath(); // Ground Floor Left Front
+ctx.moveTo(backX1,backY1);
+ctx.lineTo(backX1 + ((Xwidth/2)-10),backY1);
+ctx.lineTo(backX1 + ((Xwidth/2)-10),backY1 + 50);
+ctx.lineTo(backX1,backY1 + 50);
+ctx.lineTo(backX1,backY1);
+ctx.stroke();
+
+    drawRooms();
+}
+
+function drawRooms(){
+    ctx.strokeStyle = "grey";
+    ctx.lineWidth = 2;
+/// Ground Floor
+    ctx.beginPath(); // Ground Floor Left Front
+    ctx.moveTo(topXCorner + 10,topYCorner + ((Yheight/2) - 10));
+    ctx.lineTo(topXCorner + ((Xwidth/2)-10),topYCorner + ((Yheight/2) - 10));
+    ctx.lineTo(topXCorner + ((Xwidth/2)-10),topYCorner + Yheight - 10);
+    ctx.lineTo(topXCorner + 10,topYCorner + Yheight - 10);
+    ctx.lineTo(topXCorner + 10,topYCorner + ((Yheight/2) - 10));
+    ctx.stroke();
+
+    ctx.beginPath(); // Ground Floor Front
+    ctx.moveTo(topXCorner + Xwidth - 10, topYCorner + ((Yheight/2) - 10));
+    ctx.lineTo(topXCorner + Xwidth - ((Xwidth/2)-10),topYCorner + ((Yheight/2) - 10));
+    ctx.lineTo(topXCorner + Xwidth - ((Xwidth/2)-10),topYCorner + Yheight - 10);
+    ctx.lineTo(topXCorner + Xwidth - 10, topYCorner + Yheight - 10);
+    ctx.lineTo(topXCorner + Xwidth - 10, topYCorner + ((Yheight/2) - 10));
+    ctx.stroke();
+/// Middle Floor
+    ctx.beginPath(); // 2nd Floor Left Room
+    ctx.moveTo(topXCorner + 10, topYCorner + 10);
+    ctx.lineTo(topXCorner + ((Xwidth/3) + 50) - 10,topYCorner + 10);
+    ctx.lineTo(topXCorner + ((Xwidth/3) + 50) - 10,topYCorner + ((Yheight/2) - 20));
+    ctx.lineTo(topXCorner + 10, topYCorner + ((Yheight/2) - 20));
+    ctx.lineTo(topXCorner + 10, topYCorner + 10);
+    ctx.stroke();
+
+    ctx.beginPath(); // 2nd Floor Middle Room
+    ctx.moveTo(topXCorner + ((Xwidth/3) + 50), topYCorner + 10);
+    ctx.lineTo(topXCorner + Xwidth - ((Xwidth/3)+50),topYCorner + 10);
+    ctx.lineTo(topXCorner + Xwidth - ((Xwidth/3)+50),topYCorner + ((Yheight/2) - 20));
+    ctx.lineTo(topXCorner + ((Xwidth/3) + 50), topYCorner + ((Yheight/2) - 20));
+    ctx.lineTo(topXCorner + ((Xwidth/3) + 50), topYCorner + 10);
+    ctx.stroke();
+
+    ctx.beginPath(); // 2nd Floor Right Room
+    ctx.moveTo(topXCorner + Xwidth - 10, topYCorner + 10);
+    ctx.lineTo(topXCorner + Xwidth - ((Xwidth/3)+50) + 10,topYCorner + 10);
+    ctx.lineTo(topXCorner + Xwidth - ((Xwidth/3)+50) + 10,topYCorner + ((Yheight/2) - 20));
+    ctx.lineTo(topXCorner + Xwidth - 10, topYCorner + ((Yheight/2) - 20));
+    ctx.lineTo(topXCorner + Xwidth - 10, topYCorner + 10);
+    ctx.stroke();
+
+}
