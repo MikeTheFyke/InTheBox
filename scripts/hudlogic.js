4ce71df6368a06ea6cbbe939a975a7f690b3fc18
MikeTheFyke <mikefyke@hotmail.com>
2020-03-05 13:46:04 -0500

    Refactured hud logic, draw layers into functions

--- scripts/hudlogic.js
+++ scripts/hudlogic.js
@@ -4,10 +4,10 @@ var framesPerSecond = 30;
 
 var Xcord = 0;
 var Ycord = 0;
-var topCorner = 20;
+var topYCorner = 20;
 var topXCorner = 20;
-const xthick = 800;
-const ythick = 450;
+const xthick = 900;
+const ythick = 525;
 
 
 function calculateMousePos(evt){      // an event fires when mouse moves
@@ -29,55 +29,62 @@ window.onload = function() {
     },1000/framesPerSecond); // Hundredth of seconds, ballX movement
 
     canvas.addEventListener ('mousemove',   // keypress, mouseclick, mousemove
-      function(evt) {
-        var mousePos = calculateMousePos(evt);
-        Xcord = mousePos.x;
-        Ycord = mousePos.y;
-        topCorner = mousePos.y-(ythick/2); // paddle aligns in centre with mouse
-        topXCorner = mousePos.x-(xthick/2);
-        redTopY = topCorner / 2;
-        redTopX = topXCorner / 2;
-        yellTopX = (topXCorner / 2) + 703;
-        yellTopY = topCorner / 2;
-       });
+        function(evt) {
+            var mousePos = calculateMousePos(evt);
+            Xcord = mousePos.x;
+            Ycord = mousePos.y;
+            topYCorner = mousePos.y-((ythick/2) + 75);
+            topXCorner = mousePos.x-((xthick/2) + 50);
+            // redTopY = topYCorner / 2;
+            // redTopX = topXCorner / 2;
+            // yellTopX = (topXCorner / 2) + 703;
+            // yellTopY = topYCorner / 2;
+       }
+    );
 }
 
 function drawEverything() {
     ctx.fillStyle = "black"; // Canvas Black
     ctx.fillRect(0,0,canvas.width,canvas.height);
-            if (topCorner <= 50){ // Top Boundry
-                topCorner = 50 ;}
-            if (topCorner >= 150){ // Bottom Boundry
-                topCorner = 150 ;} 
+    drawRect()
+    drawLines()
+  }
+
+  function drawRect() {
+            if (topYCorner <= 50){ // Top Boundry
+                topYCorner = 50 ;}
+            if (topYCorner >= 150){ // Bottom Boundry
+                topYCorner = 150 ;} 
             if (topXCorner >= 550){ // Right Boundry
                 topXCorner = 550 ;}
             if (topXCorner <= 50){ // Left Boundry
                 topXCorner = 50 ;}
     ctx.fillStyle = "teal"; // Back Wall Grey Border
-    ctx.fillRect(topXCorner,topCorner,xthick,ythick);
+    ctx.fillRect(topXCorner,topYCorner,xthick,ythick);
     ctx.fillStyle = "black"; // Back Wall Black
-    ctx.fillRect(topXCorner +5,topCorner +5 ,790,440);
-    
+    ctx.fillRect(topXCorner +5,topYCorner +5 ,(xthick -10),(ythick -10));
+  }
+
+  function drawLines() {
     ctx.strokeStyle = "teal";
     ctx.beginPath(); // Top Left Line
     ctx.lineWidth = 2;
     ctx.moveTo(0,0);
-    ctx.lineTo(topXCorner,topCorner);
-    
+    ctx.lineTo(topXCorner,topYCorner);
     ctx.stroke();
+
     ctx.beginPath(); // Top Right Line
     ctx.moveTo(1400,0);
-    ctx.lineTo(topXCorner + xthick,topCorner);
-    
+    ctx.lineTo(topXCorner + xthick,topYCorner);
     ctx.stroke();
+
     ctx.beginPath(); // Bottom Right Line
     ctx.moveTo(0,675);
-    ctx.lineTo(topXCorner,topCorner + ythick);
-    
+    ctx.lineTo(topXCorner,topYCorner + ythick);
     ctx.stroke();
+
     ctx.beginPath(); // Bottom Left Line
     ctx.moveTo(1400,675);
-    ctx.lineTo(topXCorner + xthick,topCorner +ythick);
-    
+    ctx.lineTo(topXCorner + xthick,topYCorner +ythick); 
     ctx.stroke();
   }
