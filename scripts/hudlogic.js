b096c3f596c5296d3f50f25671dfcf6a2b4a9da1
MikeTheFyke <mikefyke@hotmail.com>
2020-03-05 15:06:33 -0500

    botom icon is now loading

--- scripts/hudlogic.js
+++ scripts/hudlogic.js
@@ -125,5 +125,23 @@ function drawEverything() {
   }
 
   function drawIcons () {
+    ctx.strokeStyle = "orange";
+    ctx.beginPath(); // Top Middle Icon
+    ctx.lineWidth = 5;
+    ctx.moveTo((topXCorner + 270),topYCorner); // LeTop Mid Top Corner
+    ctx.lineTo( ( (topXCorner + xthick) - 270),topYCorner); // RT Mid Top Corner
+    ctx.lineTo( ( (topXCorner + xthick) - 285),topYCorner + 15); // RT SubMid
+    ctx.lineTo((topXCorner + 285),topYCorner + 15); // LeTop SubMid 
+    ctx.closePath();
+    ctx.stroke();
 
+    ctx.strokeStyle = "orange";
+    ctx.beginPath(); // Top Middle Icon
+    ctx.lineWidth = 5;
+    ctx.moveTo( ((topXCorner + xthick) - 270),topYCorner + ythick); // RiBot Mid Corner
+    ctx.lineTo((topXCorner + 270),(topYCorner + ythick)); // LeBot Mid Corner
+    ctx.lineTo((topXCorner + 285),((topYCorner + ythick) - 15 ) ); // LeBot SubMid
+    ctx.lineTo( ((topXCorner + xthick) - 285),(topYCorner + ythick) -15); // RiBto SubMid
+    ctx.closePath();
+    ctx.stroke();
   }
