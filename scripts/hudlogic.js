2615af11e7ea5a2ac1d7bb2c12ff234d5cf6fa63
MikeTheFyke <mikefyke@hotmail.com>
2020-03-05 19:23:59 -0500

    More Arcs logic in hud

--- scripts/hudlogic.js
+++ scripts/hudlogic.js
@@ -170,20 +170,20 @@ function drawArc () {
     // context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
     ctx.strokeStyle = "blue"
     ctx.beginPath();
-    ctx.moveTo( (topXCorner) - 25,topYCorner + 125); // LeTop Point 02
+    ctx.moveTo((topXCorner) - 25,(topYCorner + ythick) - 125);  // LeBot Point 02
+    ctx.arc(topXCorner - 25, topYCorner + ( ythick/2 ), 135, 90, 1.5 * Math.PI, false);
+    
+    ctx.lineTo( (topXCorner) - 25,topYCorner + 125); // LeTop Point 02
     ctx.arc(topXCorner + 10, topYCorner + ( ( ythick/2)/2 ) - 30 , 100, 110, 1.5 * Math.PI, false);
     ctx.lineTo((topXCorner),topYCorner); // Left Top Corner
     ctx.lineTo((topXCorner + 250),topYCorner); // LeTop Mid Top Corner
-    ctx.lineTo((topXCorner + 275),topYCorner + 25); // LeTop SubMid 
+    ctx.lineTo((topXCorner + 275),topYCorner + 25); // LeTop SubMid
+    ctx.lineTo( ( (topXCorner + xthick) - 275),topYCorner + 25); // RT SubMid 
+    ctx.lineTo( ( (topXCorner + xthick) - 250),topYCorner); // RT Mid Top Corner
+    ctx.lineTo( (topXCorner + xthick),topYCorner);  // Right Top Corner
+    ctx.arc((topXCorner + xthick) - 10, topYCorner + ( ( ythick/2)/2 ) - 30 , 100, 1.5 * Math.PI, 0, false);
+    ctx.lineTo( ((topXCorner + xthick) + 25),topYCorner + 125); // RT Point 2
     ctx.stroke()
 
-    ctx.beginPath();
-    ctx.moveTo((topXCorner) - 25,(topYCorner + ythick) - 125);  // LeBot Point 02
-    ctx.arc(topXCorner + 10, topYCorner + ( ( ythick/2)*2 ) - 30 , 100,  1.5 * Math.PI,110, true);
-    ctx.lineTo((topXCorner),(topYCorner + ythick) );  // Left Bottom Corner
-    ctx.lineTo((topXCorner + 250),(topYCorner + ythick)); // LeBot Mid Corner
-    ctx.lineTo((topXCorner + 275),((topYCorner + ythick) - 25 ) ); // LeBot SubMid
-
-    ctx.stroke()
 
 }
