833a326419a04144e553ff1f062e0e44b5261385
MikeTheFyke <mikefyke@hotmail.com>
2020-03-10 15:10:51 -0400

    Added bullet counter bullets

--- scripts/hud/ammo.js
+++ scripts/hud/ammo.js
@@ -21,20 +21,35 @@ function ammoBar(){
     ctx.closePath();
     ctx.stroke();
 
-    drawBars()
+    // drawBars() Health Like Interior Bar
+    drawBullets()
 
     ctx.font = "30px Verdana";
     ctx.fillText(ammoText,topXCorner + 180,topYCorner + 30, 90,100);
 }
 
-function drawBars() {
-    ctx.strokeStyle = "green";
-    ctx.beginPath(); // Health Icon
-    ctx.lineWidth = 5;
-    ctx.moveTo( topXCorner + 10,topYCorner + 25); // LeTop Mid Top Corner
-    ctx.lineTo( topXCorner + 195,topYCorner + 5);      // RT Mid Top Corner
-    ctx.lineTo( topXCorner + 220,topYCorner + 30); // RT SubMid
-    ctx.lineTo( topXCorner + 10,topYCorner + 30); // LeTop SubMid 
-    ctx.closePath();
-    ctx.stroke();
+// function drawBars() { // Health Like Bar
+//     ctx.strokeStyle = "green";
+//     ctx.beginPath(); // Health Icon
+//     ctx.lineWidth = 5;
+//     ctx.moveTo( topXCorner + 10,topYCorner + 25); // LeTop Mid Top Corner
+//     ctx.lineTo( topXCorner + 195,topYCorner + 5);      // RT Mid Top Corner
+//     ctx.lineTo( topXCorner + 220,topYCorner + 30); // RT SubMid
+//     ctx.lineTo( topXCorner + 10,topYCorner + 30); // LeTop SubMid 
+//     ctx.closePath();
+//     ctx.stroke();
+// }
+
+function drawBullets(){
+    for (var i = 0; i < ammoCount; i++){
+        ctx.strokeStyle = "blue";
+        ctx.beginPath(); // Top Middle Icon
+        ctx.lineWidth = 2;
+        ctx.moveTo( topXCorner + (i * 10) + (i*10),topYCorner + 50); // LeTop Mid Top Corner
+        ctx.lineTo( topXCorner + ((i * 10) + 10) + (i*10),topYCorner + 50);      // RT Mid Top Corner
+        ctx.lineTo( topXCorner + ((i * 10) + 10) + (i*10),topYCorner + 65); // RT SubMid
+        ctx.lineTo( topXCorner + (i * 10) + (i*10),topYCorner + 65); // LeTop SubMid 
+        ctx.closePath();
+        ctx.stroke();
+    }
 }
