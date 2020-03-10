d66e2f123eb8186b099803f0a95633c16a542095
MikeTheFyke <mikefyke@hotmail.com>
2020-03-10 12:07:00 -0400

    adjusted cornering of arc hud

--- scripts/hudlogic.js
+++ scripts/hudlogic.js
@@ -173,14 +173,14 @@ function drawArc () {
     //--------------------Right Side Arcs
     ctx.arc((topXCorner),(topYCorner + ythick) -100, 100, .5 * Math.PI, 1.0 * Math.PI, false); // Bottom Left Arc
     ctx.arc(topXCorner - 25, topYCorner + ( ythick/2 ), 135, .5 * Math.PI, 1.5 * Math.PI, false); // Left Middle Arc
-    ctx.arc(topXCorner + 10, topYCorner + ( ( ythick/2)/2 ) - 30 , 100, 1.0 * Math.PI, 1.5 * Math.PI, false); // Top Left Arc
+    ctx.arc(topXCorner + 10, topYCorner + ( ( ythick/2)/2 ) - 35 , 100, 1.0 * Math.PI, 1.5 * Math.PI, false); // Top Left Arc
     //--------------------Middle Top Bars
     ctx.lineTo((topXCorner + 220),topYCorner - 30 ); // LeTop Mid Top Corner
     ctx.lineTo((topXCorner + 275),topYCorner + 25); // LeTop SubMid
     ctx.lineTo( ( (topXCorner + xthick) - 275),topYCorner + 25); // RT SubMid 
     ctx.lineTo( ( (topXCorner + xthick) - 220),topYCorner - 30); // RT Mid Top Corner
     //--------------------Leftt Side Arcs
-    ctx.arc((topXCorner + xthick) - 10, topYCorner + ( ( ythick/2)/2 ) - 30 , 100, 1.5 * Math.PI, 0, false); // Top Right Arc
+    ctx.arc((topXCorner + xthick) - 10, topYCorner + ( ( ythick/2)/2 ) - 35 , 100, 1.5 * Math.PI, 0, false); // Top Right Arc
     ctx.arc( ((topXCorner + xthick) + 25), topYCorner + ( ythick/2 ), 135, 1.5 * Math.PI, .5 * Math.PI, false); // Right Middle Arc
     ctx.arc( ((topXCorner + xthick) - 10),(topYCorner + ythick) -100, 100, 0 * Math.PI, .5 * Math.PI, false); // Bottom right Arc
     //--------------------Middle Bottom Bars
