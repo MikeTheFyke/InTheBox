ae733ec3bf0bf25b5b64d52a4473eb1cfa4095bd
MikeTheFyke <mikefyke@hotmail.com>
2020-03-10 18:32:26 -0400

    Refacturing health bar logic

--- scripts/hud/heathBar.js
+++ scripts/hud/heathBar.js
@@ -1,11 +1,38 @@
-// function drawHealthBar() {
-//     ctx.strokeStyle = "white";
-//     ctx.beginPath(); // main frame
-//     ctx.lineWidth = 5;
-//     ctx.moveTo( topXCorner, topYCorner + 20 ); // LeTop Mid Top Corner
-//     ctx.lineTo( topXCorner + 200, topYCorner - 5 );      // RT Mid Top Corner
-//     ctx.lineTo( topXCorner + 240, topYCorner + 40 ); // RT SubMid
-//     ctx.lineTo( topXCorner, topYCorner + 40 ); // LeTop SubMid 
-//     ctx.closePath();
-//     ctx.stroke();
-// }
+function drawHealthBar() {
+    ctx.fillStyle = "white";
+    ctx.beginPath(); // main frame
+    ctx.lineWidth = 5;
+    ctx.moveTo( topXCorner + xthick, topYCorner + 20 ); // Left Top Corner
+    ctx.lineTo( topXCorner + xthick - 200, topYCorner - 5 ); // Right Top Corner
+    ctx.lineTo( topXCorner + xthick - 240, topYCorner + 40 ); // Right Bottom Corner
+    ctx.lineTo( topXCorner + xthick, topYCorner + 40 ); // Left Bottom Corner 
+    ctx.closePath();
+    ctx.fill();
+
+    var healthBarWidth = ( (topXCorner + xthick) - (topXCorner + xthick - 240) ) / 8;
+    console.log("health Bar Width = " + healthBarWidth);
+
+    for (c = 0; c <= 7; c++) {   
+        ctx.strokeStyle = "darkblue";
+        ctx.fillStyle = "blue";
+        ctx.beginPath()
+        ctx.moveTo( topXCorner + xthick - ( c * healthBarWidth ) + ( c * 5 ) , topYCorner + 20 - ( c * 3 ) ); // Right Top
+        ctx.lineTo( topXCorner + xthick - ( c * healthBarWidth ), topYCorner + 40 ); // Right Bottom
+        ctx.lineTo(topXCorner + xthick - ( healthBarWidth ) - ( c * healthBarWidth ) - ( c * .75 ) , topYCorner + 40 ) // Left Bottom
+        ctx.lineTo(topXCorner + xthick - ( healthBarWidth ) - ( c * healthBarWidth ) + ( c * 5.5 ) , topYCorner + 20  - ( c * 4 ) ) // Left Top
+        ctx.closePath()
+        ctx.stroke()
+        ctx.fill()
+    }
+
+    ctx.strokeStyle = "darkblue";
+    ctx.beginPath(); // main frame
+    ctx.lineWidth = 6;
+    ctx.moveTo( topXCorner + xthick, topYCorner + 20 ); // Left Top Corner
+    ctx.lineTo( topXCorner + xthick - 200, topYCorner - 5 ); // Right Top Corner
+    ctx.lineTo( topXCorner + xthick - 240, topYCorner + 40 ); // Right Bottom Corner
+    ctx.lineTo( topXCorner + xthick, topYCorner + 40 ); // Left Bottom Corner 
+    ctx.closePath();
+    ctx.stroke();
+
+}
--- scripts/hudlogic.js
+++ scripts/hudlogic.js
@@ -56,6 +56,7 @@ function drawEverything() {
     rectile()
     moveBar()
     drawHealthBar()
+    
   }
 
   function drawRect() {
@@ -195,15 +196,40 @@ function blueHud () {
 }
 
 function drawHealthBar() {
-    ctx.strokeStyle = "white";
+    ctx.fillStyle = "white";
     ctx.beginPath(); // main frame
     ctx.lineWidth = 5;
-    ctx.moveTo( topXCorner + xthick, topYCorner + 20 ); // LeTop Mid Top Corner
-    ctx.lineTo( topXCorner + xthick - 200, topYCorner - 5 );      // RT Mid Top Corner
-    ctx.lineTo( topXCorner + xthick - 240, topYCorner + 40 ); // RT SubMid
-    ctx.lineTo( topXCorner + xthick, topYCorner + 40 ); // LeTop SubMid 
+    ctx.moveTo( topXCorner + xthick, topYCorner + 20 ); // Left Top Corner
+    ctx.lineTo( topXCorner + xthick - 200, topYCorner - 5 ); // Right Top Corner
+    ctx.lineTo( topXCorner + xthick - 240, topYCorner + 40 ); // Right Bottom Corner
+    ctx.lineTo( topXCorner + xthick, topYCorner + 40 ); // Left Bottom Corner 
+    ctx.closePath();
+    ctx.fill();
+
+    var healthBarWidth = ( (topXCorner + xthick) - (topXCorner + xthick - 240) ) / 8;
+    console.log("health Bar Width = " + healthBarWidth);
+
+    for (c = 0; c <= 7; c++) {   
+        ctx.strokeStyle = "darkblue";
+        ctx.fillStyle = "blue";
+        ctx.beginPath()
+        ctx.moveTo( topXCorner + xthick - ( c * healthBarWidth ) + ( c * 5 ) , topYCorner + 20 - ( c * 3 ) ); // Right Top
+        ctx.lineTo( topXCorner + xthick - ( c * healthBarWidth ), topYCorner + 40 ); // Right Bottom
+        ctx.lineTo(topXCorner + xthick - ( healthBarWidth ) - ( c * healthBarWidth ) - ( c * .75 ) , topYCorner + 40 ) // Left Bottom
+        ctx.lineTo(topXCorner + xthick - ( healthBarWidth ) - ( c * healthBarWidth ) + ( c * 5.5 ) , topYCorner + 20  - ( c * 4 ) ) // Left Top
+        ctx.closePath()
+        ctx.stroke()
+        
+    }
+
+    ctx.strokeStyle = "darkblue";
+    ctx.beginPath(); // main frame
+    ctx.lineWidth = 6;
+    ctx.moveTo( topXCorner + xthick, topYCorner + 20 ); // Left Top Corner
+    ctx.lineTo( topXCorner + xthick - 200, topYCorner - 5 ); // Right Top Corner
+    ctx.lineTo( topXCorner + xthick - 240, topYCorner + 40 ); // Right Bottom Corner
+    ctx.lineTo( topXCorner + xthick, topYCorner + 40 ); // Left Bottom Corner 
     ctx.closePath();
     ctx.stroke();
-}
 
-ctx.lineTo( (topXCorner + xthick),topYCorner);  // Right Top Corner
+}
