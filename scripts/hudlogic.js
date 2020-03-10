aead5f38143047fae3df15120e6a12b53392e763
MikeTheFyke <mikefyke@hotmail.com>
2020-03-10 13:17:32 -0400

    Created directory hudlogic

--- scripts/hudlogic.js
+++ scripts/hudlogic.js
@@ -251,17 +251,17 @@ function flashing(){
             console.log("ctr = " + ctr)
         }
     } else {
-        for (var a = 0; a <= 50; a ++){
+        for (var b = 0; b <= 50; b ++){
             ctx.strokeStyle="white"
             ctx.lineWidth = 1;
             ctx.beginPath();
-            ctx.arc( topXCorner,(topYCorner + ythick) - 62.5, (50 - a), 0 * Math.PI, 2.0 * Math.PI, false); // Middle Point
+            ctx.arc( topXCorner,(topYCorner + ythick) - 62.5, (50 - b), 0 * Math.PI, 2.0 * Math.PI, false); // Middle Point
             ctx.stroke()
 
             ctx.strokeStyle="blue"
             ctx.lineWidth = 1;
             ctx.beginPath();
-            ctx.arc( topXCorner,(topYCorner + ythick) - 62.5, (50 - a), 0 * Math.PI, 2.0 * Math.PI, false); // Middle Point
+            ctx.arc( topXCorner,(topYCorner + ythick) - 62.5, (50 - b), 0 * Math.PI, 2.0 * Math.PI, false); // Middle Point
             ctx.stroke()
         
             ctr = ctr - 1
