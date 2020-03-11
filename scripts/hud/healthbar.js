6bebf7e095c5270cc1f6dd6b58dc11f65bc0508f
MikeTheFyke <mikefyke@hotmail.com>
2020-03-11 13:58:19 -0400

    More health bar reacturing

--- scripts/hud/healthbar.js
+++ scripts/hud/healthbar.js
@@ -1,20 +1,29 @@
 function drawHealthBar() {
     ctx.lineWidth = 6;
-    ctx.strokeStyle = "darkblue";
-    ctx.fillStyle = "white";
+    ctx.strokeStyle = "blue";
+    ctx.fillStyle = "lightblue";
     ctx.beginPath(); // main frame
     ctx.lineWidth = 5;
 
     ctx.beginPath();
     ctx.arc(topXCorner + xthick -220 , topYCorner + 30, 20, 0.5 * Math.PI, 1.25 * Math.PI, false); // Bottom Left Arc
     ctx.arc(topXCorner + xthick -200 , topYCorner + 10, 20, 1.25 * Math.PI, 1.5 * Math.PI, false); // Top Left Arc
-    ctx.arc(topXCorner + xthick + 20 , topYCorner + 40, 20, 1.5 * Math.PI, 2 * Math.PI, false); // Top Right Arc
-    ctx.lineTo(topXCorner + xthick + 40 , topYCorner + 70); // Bottom Right Corner
-    ctx.lineTo(topXCorner + xthick + 30 , topYCorner + 70); // Bottom Left Corner
-    ctx.arc(topXCorner + xthick + 5 , topYCorner + 70, 20, 0 * Math.PI, 1.5 * Math.PI, true); // Bottom Interior Arc
+    ctx.arc(topXCorner + xthick + 20 , topYCorner + 65, 40, 1.5 * Math.PI, 2.25 * Math.PI, false); // Top Right Arc
+    ctx.arc(topXCorner + xthick + 20 , topYCorner + 65, 40, 0 * Math.PI, .5 * Math.PI, false); // Top Right Arc
+    ctx.arc(topXCorner + xthick - 30 , topYCorner + 100, 40, 0 * Math.PI, 1.5 * Math.PI, true); // Bottom Interior Arc
+    ctx.closePath();
+    // ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(topXCorner + xthick -210 , topYCorner + 27, 15, 0.5 * Math.PI, 1.25 * Math.PI, false); // Bottom Left Arc
+    ctx.arc(topXCorner + xthick -200 , topYCorner + 13, 15, 1.25 * Math.PI, 1.5 * Math.PI, false); // Top Left Arc
+    ctx.arc(topXCorner + xthick + 17 , topYCorner + 63, 35, 1.5 * Math.PI, 2.25 * Math.PI, false); // Top Right Arc
+    ctx.arc(topXCorner + xthick + 17 , topYCorner + 63, 35, 0 * Math.PI, .5 * Math.PI, false); // Top Right Arc
+    ctx.arc(topXCorner + xthick - 18 , topYCorner + 90, 35, 0 * Math.PI, 1.5 * Math.PI, true); // Bottom Interior Arc
     ctx.closePath();
     ctx.fill();
-    ctx.stroke();
+    
 
     // var healthBarWidth = ( (topXCorner + xthick) - (topXCorner + xthick - 240) ) / 8;
     // console.log("health Bar Width = " + healthBarWidth);
